ee6710bf8d41aa891dfc0e4893faf6c7e194332a
Tylar Murray <murray.tylar@gmail.com>
2019-06-06 14:48:21 +0000

    allow separate DESCRIPTION files for each content page

--- install_R_deps.sh
+++ install_R_deps.sh
@@ -2,8 +2,15 @@ set -e
 
 Rscript -e 'if (!requireNamespace("devtools")) install.packages("devtools")'
 
-# install Imports from DESCRIPTION
+# install global Imports from DESCRIPTION
 Rscript -e 'devtools::install()'
+
+# install Imports for each content page
+echo "installing dependencies for content pages. DESCRIPTION files found in:"
+find ./content -type f -name 'DESCRIPTION' -print0 | xargs -0 echo "$(sed -r 's|/[^/]+$||' )"
+
+find ./content -type f -name 'DESCRIPTION' -print0 | \
+    xargs -0 Rscript -e "devtools::install( \"$(sed -r 's|/[^/]+$||')\")"
 
 # blogdown setup [[ref](https://bookdown.org/yihui/blogdown/installation.html)]
 # Rscript -e 'devtools::install_github("rstudio/blogdown")'
