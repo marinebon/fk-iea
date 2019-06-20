192cfa77670dbc9df91de0cf7ffe5d2d360ca2b2
Tylar Murray <murray.tylar@gmail.com>
2019-06-20 20:16:29 +0000

    + --user as suggested by pip

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -1,12 +1,12 @@
 set -e
 
 echo "install from ./requirements.txt"
-python3 -m pip install -r requirements.txt
+python3 -m pip install --user -r requirements.txt
 
 # install Imports for each content page
 echo "installing dependencies for content pages with requrements.txt files..."
 find ./content -type f -name 'requirements.txt' -printf '%h\n' |
     while read line; do
         printf "\n=== === ===\n\t$line\n=== === ===\n";
-        python3 -m pip install -r $line
+        python3 -m pip install --user -r $line
     done
