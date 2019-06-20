15ea0492ac94c9b94c635349630f7545d2369122
Tylar Murray <murray.tylar@gmail.com>
2019-06-20 17:56:07 +0000

    pip installs speak up

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -1,12 +1,12 @@
 set -e
 
 echo "install from ./requirements.txt"
-pip3 install -q -r requirements.txt
+pip3 install -r requirements.txt
 
 # install Imports for each content page
 echo "installing dependencies for content pages with requrements.txt files..."
 find ./content -type f -name 'requirements.txt' -printf '%h\n' |
     while read line; do
         printf "\n=== === ===\n\t$line\n=== === ===\n";
-        pip3 install -q -r $line
+        pip3 install -r $line
     done
