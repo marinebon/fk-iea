148ffb98addd6058d164d0d4cd5203bac3b04543
Tylar Murray <murray.tylar@gmail.com>
2019-06-20 17:46:55 +0000

    use pip3

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -1,12 +1,12 @@
 set -e
 
-# install global Imports from requirements.txt
-pip install -q -r requirements.txt
+echo "install from ./requirements.txt"
+pip3 install -q -r requirements.txt
 
 # install Imports for each content page
-echo "\n\ninstalling dependencies for content pages with requrements.txt files..."
+echo "installing dependencies for content pages with requrements.txt files..."
 find ./content -type f -name 'requirements.txt' -printf '%h\n' |
     while read line; do
         printf "\n=== === ===\n\t$line\n=== === ===\n";
-        pip install -q -r $line
+        pip3 install -q -r $line
     done
